7627ce325cf672cb4c9518ed8ac9371360b38e44
Lakshman Singh <lslakshman2024@gmail.com>
2025-07-04 22:10:45 +0530

    import changed

--- components/student/AttemptQuiz.tsx
+++ components/student/AttemptQuiz.tsx
@@ -6,16 +6,13 @@ import styles from '@/styles/student/AttempQuiz.module.css';
 import { Question, Result } from '@/types/types';
 import { useRouter } from 'next/router';
 import React, { useEffect, useRef, useState } from 'react';
-
-// import { addIsSelectedFieldInOptions, fillIsSelectedAsTrueForClickedOption, giveQuizResult, shuffleArray } from '@/services/Result';
 import Loader from '../common/Loader';
 import QuestionOptions from './cards/QuestionOptions';
 import TimeCard from './cards/TimeCard';
 import ConfirmModal from './modals/ConfirmModal';
 import ResultModal from './modals/ResultModal';
-import { setLocalStorage } from '@/services/CommonServices';
-// import { addIsSelectedFieldInOptions, fillIsSelectedAsTrueForClickedOption, giveQuizResult, shuffleArray, shuffleOptions} from '@/services/Result';
-import { addIsSelectedFieldInOptions, fillIsSelectedAsTrueForClickedOption, giveQuizResult, shuffleArray, shuffleOptions} from '../../services/Result';
+import { setLocalStorage } from '@/services/CommonServices'; 
+import { addIsSelectedFieldInOptions, fillIsSelectedAsTrueForClickedOption, giveQuizResult, shuffleArray, shuffleOptions } from '@/services/result';
 
 
 const AttempQuiz: React.FC = () => {
